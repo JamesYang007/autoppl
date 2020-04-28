3c22ade699ef8bd4b649f2d586162cbdc0b2a8aa
James Yang <jamesyang916@gmail.com>
2020-04-27 23:43:07 -0400

    Remove sudo

--- setup.sh
+++ setup.sh
@@ -32,6 +32,6 @@ fi
 cd /tmp
 git clone --recurse-submodules https://github.com/JamesYang007/FastAD.git FastAD
 cd FastAD
-sudo ./setup.sh
-sudo ./install.sh
+./setup.sh
+./install.sh
 cd $projectdir
