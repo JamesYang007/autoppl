3ebbaa2107c9a201a80ad3a1d8c91a08f5f7aaf0
James Yang <jamesyang916@gmail.com>
2020-04-27 22:48:43 -0400

    Fix build for CI

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+projectdir=$(dirname "BASH_SOURCE")
 gbenchpath="lib/benchmark"
 gtestpath="$gbenchpath/googletest"
 
@@ -12,4 +13,17 @@ if [ ! -d "$gtestpath" ]; then
     cd $gtestpath 2>&1 /dev/null
     git checkout -q release-1.10.0
     cd ~- # change back to previous dir and no output to terminal
+fi
+
+# install Armadillo only on Linux
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    sudo apt install libopenblas-dev liblapack-dev
+    cd /tmp
+    wget http://sourceforge.net/projects/arma/files/armadillo-9.870.2.tar.gz
+    tar -xvf armadillo-9.870.2.tar.gz
+    cd armadillo-9.870.2
+    cmake .
+    make
+    sudo make install
+    cd $projectdir
 fi
